5b66e243e18cc840260ac41338ce219520b1ea2e
Ihor Radchuk <radchuk2808@gmail.com>
2018-05-04 17:17:13 +0300

    update

--- front-page.php
+++ front-page.php
@@ -1,5 +1,10 @@
 <?php get_header();
-global $post;
-?>
-
+global $post; ?>
+<section id="content">
+    <div class="row">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
+    </div>
+</section>
 <?php get_footer(); ?>
