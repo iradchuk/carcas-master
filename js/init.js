3ba568ea5613e3dc5804ad97d447a610f65ea7b5
Ihor Radchuk <radchuk2808@gmail.com>
2018-05-28 10:45:36 +0300

    select disable

--- js/init.js
+++ js/init.js
@@ -13,6 +13,13 @@ $(document).ready(function () {
 //         disable: 'mobile',
     // anchor: 'top-bottom'
 //     });
+    if (!$(".woocommerce-checkout")[0]) {
+        $("select").selbel();
+        $("select option:first-of-type").attr('selected','true').attr('disabled','disabled').attr('value','0');
+    }
+});
+/* end ready*/
+$(window).on('load', function () {
     //swiper_example
     // var swiper_example = new Swiper('.swiper_example', {
     //     loop: true,
@@ -36,4 +43,3 @@ $(document).ready(function () {
     //     }
     // });
 });
-/* end ready*/
