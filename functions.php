0d6811f138a81a2ad05560fd16a83ad558776d96
Ihor Radchuk <radchuk2808@gmail.com>
2018-10-04 14:27:01 +0300

    update

--- functions.php
+++ functions.php
@@ -35,3 +35,15 @@ register_nav_menus( array(
 	'foot_menu'   => 'Footer navigation'
 ) );
 
+function wp_get_attachment( $attachment_id ) {
+
+	$attachment = get_post( $attachment_id );
+	return array(
+		'alt' => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+		'caption' => $attachment->post_excerpt,
+		'description' => $attachment->post_content,
+		'href' => get_permalink( $attachment->ID ),
+		'src' => $attachment->guid,
+		'title' => $attachment->post_title
+	);
+}
