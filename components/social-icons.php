6d962fe306f1850738e56c872385448dc015e93c
Ihor Radchuk <radchuk2808@gmail.com>
2019-02-15 11:30:38 +0200

    Update social-icons.php

--- components/social-icons.php
+++ components/social-icons.php
@@ -1,24 +1,24 @@
 <?php
 if ( have_rows( 'social_icons', 'options' ) ):
-	while ( have_rows( 'social_icons', 'options' ) ) : the_row();
-		if ( get_row_layout() == 'facebook' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="icon-facebook" title="Official Facebook page"
+   while ( have_rows( 'social_icons', 'options' ) ) : the_row();
+      if ( get_row_layout() == 'facebook' ): ?>
+            <a href="<?php the_sub_field( 'url' ); ?>" class="i-fac" title="Official Facebook page"
                target="_blank"></a>
-		<?php elseif ( get_row_layout() == 'twitter' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="icon-twitter" title="Official Twitter accounts"
+      <?php elseif ( get_row_layout() == 'twitter' ): ?>
+            <a href="<?php the_sub_field( 'url' ); ?>" class="i-twi" title="Official Twitter accounts"
                target="_blank"></a>
-		<?php elseif ( get_row_layout() == 'instagram' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="icon-instagram" title="Official Instagram accounts" 
+      <?php elseif ( get_row_layout() == 'instagram' ): ?>
+            <a href="<?php the_sub_field( 'url' ); ?>" class="i-ins" title="Official Instagram accounts" 
                target="_blank"></a>
-		<?php elseif ( get_row_layout() == 'linkedin' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="icon-linkedin" title="Official Linkedin profile"
+      <?php elseif ( get_row_layout() == 'linkedin' ): ?>
+            <a href="<?php the_sub_field( 'url' ); ?>" class="i-lin" title="Official Linkedin profile"
                target="_blank"></a>
-		<?php elseif ( get_row_layout() == 'youtube' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="icon-youtube" title="Official Youtube channel"
+      <?php elseif ( get_row_layout() == 'youtube' ): ?>
+            <a href="<?php the_sub_field( 'url' ); ?>" class="i-youtube-alt" title="Official Youtube channel"
                target="_blank"></a>
-		<?php endif;
-	endwhile;
+      <?php endif;
+   endwhile;
 else :
-	// no layouts found
+   // no layouts found
 endif;
 ?>
