9667954456dbe791d8473207f992821bfa7f2283
Ihor Radchuk <radchuk2808@gmail.com>
2019-03-14 14:41:16 +0200

    update

--- components/social-icons.php
+++ components/social-icons.php
@@ -2,22 +2,22 @@
 if ( have_rows( 'social_icons', 'options' ) ):
    while ( have_rows( 'social_icons', 'options' ) ) : the_row();
       if ( get_row_layout() == 'facebook' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="i-fac" title="Official Facebook page"
+            <a href="<?php the_sub_field( 'url' ); ?>" class="fa fa-facebook" title="Official Facebook page"
                target="_blank"></a>
       <?php elseif ( get_row_layout() == 'twitter' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="i-twi" title="Official Twitter accounts"
+            <a href="<?php the_sub_field( 'url' ); ?>" class="fa fa-twitter" title="Official Twitter accounts"
                target="_blank"></a>
       <?php elseif ( get_row_layout() == 'instagram' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="i-ins" title="Official Instagram accounts" 
+            <a href="<?php the_sub_field( 'url' ); ?>" class="fa fa-instagram" title="Official Instagram accounts" 
                target="_blank"></a>
       <?php elseif ( get_row_layout() == 'linkedin' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="i-lin" title="Official Linkedin profile"
+            <a href="<?php the_sub_field( 'url' ); ?>" class="fa fa-linkedin" title="Official Linkedin profile"
                target="_blank"></a>
       <?php elseif ( get_row_layout() == 'youtube' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="i-youtube-alt" title="Official Youtube channel"
+            <a href="<?php the_sub_field( 'url' ); ?>" class="fa fa-youtube" title="Official Youtube channel"
                target="_blank"></a>
       <?php elseif ( get_row_layout() == 'yelp' ): ?>
-            <a href="<?php the_sub_field( 'url' ); ?>" class="i-yelp" title="Official Yelp channel"
+            <a href="<?php the_sub_field( 'url' ); ?>" class="fa fa-yelp" title="Official Yelp channel"
                target="_blank"></a>
       <?php endif;
    endwhile;
