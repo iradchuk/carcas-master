059f162d8461a7830859e3386fb00b0335f6729f
Ihor Radchuk <radchuk2808@gmail.com>
2019-04-18 12:51:20 +0300

    Update core.php

--- include/core.php
+++ include/core.php
@@ -24,8 +24,8 @@ function carc_style_js() {
 // wp_enqueue_script('googlemaps', '//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false', array(), '', FALSE);
 	wp_enqueue_script( 'libs', get_template_directory_uri() . '/js/lib.js', array( 'jquery' ), '1.0', true );
 	wp_enqueue_script( 'init', get_template_directory_uri() . '/js/init.js', array( 'jquery' ), '1.0', true );
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/scss/main.css' );
 	wp_enqueue_style( 'plugins', get_template_directory_uri() . '/scss/components/lib.css' );
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/scss/main.css' );
 //wp_enqueue_style('style', get_template_directory_uri() . '/scss/main.css?uid='.md5(uniqid(rand(),1)));
 }
 
